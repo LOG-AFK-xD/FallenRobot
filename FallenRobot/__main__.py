970c854611956a699716278d37b9aa4d825e2172
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-08-26 18:56:23 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -97,7 +97,7 @@ buttons = [
         InlineKeyboardButton(text="🔎 ɪɴʟɪɴᴇ 🔎", url=f"https://t.me/kimds_xD"),
     ],
     [  
-        InlineKeyboardButton(text="❣️ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ❣️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+        InlineKeyboardButton(text="❣️ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ❣️", url=f"https://t.me/kiara_probot?startgroup=true"),
     ], 
     
 ]
